eb491cd80f1b189578d41f664e8a0c98e9c41f37
Nick Pereira <nickpereira.np@gmail.com>
2015-11-24 15:17:26 -0600

    extracting cars with fuel <= 25%

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
-import sys
+import sys, requests, ast
 app = Flask(__name__, template_folder="templates")
 
 @app.route('/')
-def hello_world():
+def car2go():
+    r = requests.get("https://www.car2go.com/api/v2.0/vehicles?loc=austin&format=json")
+    cars = []
+    for car in r.json()['placemarks']:
+        if int(car['fuel']) <= 25:
+            cars.append(car)
+    for car in cars:
+        car['coordinates'] = ast.literal_eval(car['coordinates'])
+    print(type(cars[0]['coordinates']))
+    print(cars[0]['coordinates'])
     return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(sys.argv[1]))
+    app.run(host='0.0.0.0', port=int(sys.argv[1]), debug=True)
