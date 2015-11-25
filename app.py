1a0815d3565163e3711dd79ee11ce0ee76edacff
Nick <nickpereira.np@gmail.com>
2015-11-25 07:48:15 -0600

    structured data to prepare for use in javascript API

--- app.py
+++ app.py
@@ -6,14 +6,22 @@ app = Flask(__name__, template_folder="templates")
 def car2go():
     r = requests.get("https://www.car2go.com/api/v2.0/vehicles?loc=austin&format=json")
     cars = []
+    lats = []
+    longs = []
+    add_fuel = []
     for car in r.json()['placemarks']:
         if int(car['fuel']) <= 25:
             cars.append(car)
     for car in cars:
         car['coordinates'] = ast.literal_eval(car['coordinates'])
-    print(type(cars[0]['coordinates']))
-    print(cars[0]['coordinates'])
-    return render_template("index.html")
+        lats.append(car['coordinates'][0])
+        longs.append(car['coordinates'][1])
+        add_fuel.append(car['address'] + ", fuel: " + car['fuel'])
+    print(cars[0])
+    print(lats[0])
+    print(longs[0])
+    print(add_fuel[0])
+    return render_template("index.html", lats=lats, longs=longs, add_fuel=add_fuel)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(sys.argv[1]), debug=True)
