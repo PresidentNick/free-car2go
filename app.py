4e989a678d69270f7680e59da44290e6780cef40
Nick <nickpereira.np@gmail.com>
2015-11-29 20:09:35 -0500

    succesfully put markers on map

--- app.py
+++ app.py
@@ -6,22 +6,18 @@ app = Flask(__name__, template_folder="templates")
 def car2go():
     r = requests.get("https://www.car2go.com/api/v2.0/vehicles?loc=austin&format=json")
     cars = []
-    lats = []
-    longs = []
+    coords = []
     add_fuel = []
     for car in r.json()['placemarks']:
         if int(car['fuel']) <= 25:
             cars.append(car)
     for car in cars:
         car['coordinates'] = ast.literal_eval(car['coordinates'])
-        lats.append(car['coordinates'][0])
-        longs.append(car['coordinates'][1])
+        latlon = [car['coordinates'][0], car['coordinates'][1]]
+        coords.append(latlon)
         add_fuel.append(car['address'] + ", fuel: " + car['fuel'])
-    print(cars[0])
-    print(lats[0])
-    print(longs[0])
-    print(add_fuel[0])
-    return render_template("index.html", lats=lats, longs=longs, add_fuel=add_fuel)
+    return render_template("index.html", coords=coords, add_fuel=add_fuel)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(sys.argv[1]), debug=True)
+    port = sys.argv[1] if len(sys.argv) > 1 else 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
