e3b8c7ca62ce154ea819b60a00be9248cfb15e2f
Nick <nickpereira.np@gmail.com>
2015-11-29 20:14:07 -0500

    fixed int read

--- app.py
+++ app.py
@@ -19,5 +19,5 @@ def car2go():
     return render_template("index.html", coords=coords, add_fuel=add_fuel)
 
 if __name__ == '__main__':
-    port = sys.argv[1] if len(sys.argv) > 1 else 5000
+    port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
     app.run(host='0.0.0.0', port=port, debug=True)
